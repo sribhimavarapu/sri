c291d88d7beabc63fd6c710ff2c45e135ec62c02
krishnabhimavararpu <krishna.bhimavarapu@coverstack.in>
2023-03-16 22:42:52 +0530

    removed package-lock-json

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,12 +11,12 @@ const Hero = () => {
         top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
       >
         <div className="flex flex-col justify-center items-center mt-5">
-          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
-          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+          <div className="w-5 h-5 rounded-full bg-[#00cea8]" />
+          <div className="w-1 sm:h-80 h-40 green-pink-gradient" />
         </div>
         <div>
           <h1 className={`${styles.heroHeadText}`}>
-            Hi, I'm <span className="text-[#915eff]">Sri Krishna</span>
+            Hi, I'm <span className="text-[#00cea8]">Sri Krishna</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             Web developer / Front end developer
